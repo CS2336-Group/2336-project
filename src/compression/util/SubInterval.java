c3cd52b1193e89ef8474d8980eb107cc89c67427
geppettodivacin <ericdilmore@gmail.com>
2014-03-20 13:18:48 -0500

    Add SubInterval.

---
+++ src/compression/util/SubInterval.java
@@ -0,0 +1,24 @@
+class SubInterval
+{
+    private double start;
+    private double length;
+
+    public SubInterval ( int start, int length )
+    {
+        this.start = start;
+        this.length = length;
+    }
+
+    public int start()
+    {
+        return start;
+    }
+    public int end()
+    {
+        return start + length();
+    }
+    public int length()
+    {
+        return length;
+    }
+}
