bf7db901e18a5830f53e4be8278b9215df91170e
geppettodivacin <ericdilmore@gmail.com>
2014-04-19 19:32:37 -0500

    Add shell for GUI.

---
+++ src/compression/CompressionGUI.java
@@ -0,0 +1,14 @@
+package compression;
+
+public class CompressionGUI
+{
+    private CompressionGUI()
+    {
+        // Do nothing
+    }
+
+    public static void main ( String args[] )
+    {
+        new compression.gui.MainWindow();
+    }
+}
---
+++ src/compression/gui/MainWindow.java
@@ -0,0 +1,18 @@
+package compression.gui;
+
+import java.util.HashMap;
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame
+{
+    JPanel panel;
+
+    public MainWindow()
+    {
+        setTitle ( "Compression 101" );
+        setSize ( 450, 250 );
+        setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+        setVisible ( true );
+    }
+}
