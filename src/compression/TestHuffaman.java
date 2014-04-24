759160fd84e69089be09e4cff3a5a909970e91ac
geppettodivacin <ericdilmore@gmail.com>
2014-04-24 01:41:33 -0500

    Prime Huffman coder for integration.

--- src/compression/TestHuffaman.java
+++ src/compression/TestHuffaman.java
@@ -3,11 +3,8 @@ package compression;
 public class TestHuffaman {
 
 	public static void main(String[] args) {
-		HuffmanCode h=new HuffmanCode("sinega hulum peace yihonal");
-		byte[] s=h.encode();
+		HuffmanCode h=new HuffmanCode();
+		byte[] s=h.encode("This is a test");
 		h.decode(s);
-		
-		
 	}
-
 }
