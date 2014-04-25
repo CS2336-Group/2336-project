112a9498bd19c3751265774f42c99c2073d4aede
geppettodivacin <ericdilmore@gmail.com>
2014-04-25 01:33:56 -0500

    Add Javadocs to FileReader and FileWriter.

--- src/compression/util/FileReader.java
+++ src/compression/util/FileReader.java
@@ -5,6 +5,11 @@ import java.nio.file.Paths;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * A class to store static methods for reading files as both strings and as
+ * binary.
+ * @author Eric Dilmore (geppettodivacin)
+ */
 public class FileReader
 {
     private FileReader()
@@ -12,10 +17,22 @@ public class FileReader
         // Do nothing
     }
 
+    /**
+     * Read the file in from the designated filepath.
+     * @param path  the path of the file
+     * @return      a clear message as a string
+     */
     public static String readFile ( String path )
     {
         return readFile ( path, StandardCharsets.UTF_8 );
     }
+    /**
+     * Read the file in from the designated filepath with the designated
+     * encoding.
+     * @param path      the path of the file
+     * @param encoding  the charset with which to encode the String
+     * @return          a clear message as a string
+     */
     public static String readFile ( String path, Charset encoding )
     {
         byte[] encoded;
@@ -29,6 +46,11 @@ public class FileReader
         return new String ( encoded, encoding );
     }
 
+    /**
+     * Read the file from the designated filepath as an array of bytes.
+     * @param path  the path of the file
+     * @return      an encoded message as a byte array
+     */
     public static byte[] readBinary ( String path )
     {
         byte[] encoded;
